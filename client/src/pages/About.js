13a0095aca2f9d5ec7b96e5f53a11d903cb1fed9
himanshukumar29 <102627080+himanshukumar29@users.noreply.github.com>
2023-07-01 14:57:11 +0530

    commit

---
+++ client/src/pages/About.js
@@ -0,0 +1,13 @@
+import React from "react";      
+ const About = () =>{
+    return(
+        <div className='mb-20'>
+            <h1 className="sm:text-4xl text-2xl  font-bold my-6 text-gray-900">About Us</h1>
+            <p className='mx-auto leading-relaxed text-base mb-4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu f
+            </p>
+
+        </div>
+    )
+ }
+
+ export default About;
